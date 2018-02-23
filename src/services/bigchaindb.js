d75b5bf3f87038553d3caf3327e63fdcdd9f352a
Pierre Höglin <pierre.hoglin@esatto.se>
2018-02-23 17:01:26 +0100

    added cache to directions

--- src/services/bigchaindb.js
+++ src/services/bigchaindb.js
@@ -2,8 +2,6 @@ const driver = require('bigchaindb-driver');
 
 const conf = {
   API_PATH: 'https://test.bigchaindb.com/api/v1/',
-  API_ID: 'a3c2574f',
-  API_KEY: '65cdbd574760f8b259fb92f3df3248a8',
 };
 
 module.exports = {
--- src/services/directions.js
+++ src/services/directions.js
@@ -2,9 +2,15 @@ var maps = require('@google/maps').createClient({
   key: process.env.GOOGLE_API_KEY,
 });
 
+var cache = {};
+
 module.exports = {
   getLocation: address => {
     return new Promise((resolve, reject) => {
+      let cacheKey = address.replace(' ', '');
+      if (cache[cacheKey]) {
+        return resolve(cache[cacheKey]);
+      }
       maps.geocode(
         {
           address,
@@ -13,6 +19,7 @@ module.exports = {
           if (err) {
             return reject(err);
           }
+          cache[cacheKey] = response.json.results;
           resolve(response.json.results);
         },
       );
